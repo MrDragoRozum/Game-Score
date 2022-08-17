45f6d106a49a11b7caca38ed1268d1967784ac87
MrDragoRozum <roozumaleks@gmail.com>
2022-08-17 14:29:00 +0300

    Добавлены методы

--- app/src/main/java/com/example/gamescore/MainActivity.java
+++ app/src/main/java/com/example/gamescore/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.gamescore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,18 +12,72 @@ public class MainActivity extends AppCompatActivity {
     private int score1 = 0;
     private int score2 = 0;
 
+    private TextView textViewScore1;
+    private TextView textViewScore2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textViewScore1 = findViewById(R.id.textViewScore1);
-        TextView textViewScore2 = findViewById(R.id.textViewScore2);
+        Log.d("MainActivity", "onCreate() created!");
 
+        if(savedInstanceState != null) {
+            score1 = savedInstanceState.getInt("score1");
+            score2 = savedInstanceState.getInt("score2");
+        }
+
+        textViewScore1 = findViewById(R.id.textViewScore1);
+        textViewScore2 = findViewById(R.id.textViewScore2);
+
+        updateScore1(0);
+        updateScore2(0);
+
+        textViewScore1.setOnClickListener(l -> {
+            updateScore1(1);
+            Log.d("MainActivity", "Clicked textViewScore1");
+        });
+
+        textViewScore2.setOnClickListener(l -> {
+            updateScore2(1);
+            Log.d("MainActivity", "Clicked textViewScore2");
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart started!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop stopped!");
+    }
+
+    private void updateScore1(int updateScore) {
+        if(updateScore == 1) score1++;
         textViewScore1.setText(String.valueOf(score1));
-        textViewScore2.setText(String.valueOf(score2));
+    }
 
-        textViewScore1.setOnClickListener(l -> textViewScore1.setText(++score1));
-        textViewScore2.setOnClickListener(l -> textViewScore2.setText(++score2));
+    private void updateScore2(int updateScore) {
+        if(updateScore == 1) score2++;
+        textViewScore2.setText(String.valueOf(score2));
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("score1", score1);
+        outState.putInt("score2", score2);
+
+        Log.d("MainActivity", "onSaveInstanceState() saved!");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy() destroyed this activity!");
     }
 }
